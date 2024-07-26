51a7475419ab2270dbac8753c9af4426e86cd0ff
robyafrizal <robyafrizal86@gmail.com>
2024-07-26 18:31:40 +0700

    Edit config

--- auth_backend/config/postgres.js
+++ auth_backend/config/postgres.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("auth_backend", "postgres", "171586", {
+const sequelize = new Sequelize("auth_backend", "postgres", "afizfairuz87", {
   host: "127.0.0.1",
   dialect: "postgres",
 });
